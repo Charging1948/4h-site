6e3b2548d12123e1cb72d8b4272d5962f5763d90
jk <joachim.dev@pm.me>
2023-06-20 12:11:49 +0200

    update themes and stuff

--- src/config/themes.js
+++ src/config/themes.js
@@ -1,33 +1,10 @@
-// export const themes = [
-//     {
-//         light: {
-//             ...require("daisyui/src/theming/themes")["[data-theme=light]"],
-//             "primary": "#a82833",
-//             "secondary": "#7584e5",
-//             "accent": "#21a831",
-//             "neutral": "#28232f",
-//             "base-100": "#ffffff",
-//             "info": "#24aadb",
-//             "success": "#3ad9b2",
-//             "warning": "#fbd52d",
-//             "error": "#f66065",
-//         },
-//         dark: {
-//             ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
-//             "primary": "#ef5193",
-//             "secondary": "#edc89e",
-//             "accent": "#1ebc9f",
-//             "neutral": "#2a2839",
-//             "base-100": "#413e46",
-//             "info": "#8da7f7",
-//             "success": "#129b5b",
-//             "warning": "#c08011",
-//             "error": "#e35f79"
-//         },
-//     },
-// ]
-
 export const themes = [
-    "light",
-    "dark",
+    {
+        light: {
+            ...require("daisyui/src/theming/themes")["[data-theme=light]"],
+        },
+        dark: {
+            ...require("daisyui/src/theming/themes")["[data-theme=dark]"],
+        },
+    },
 ]
